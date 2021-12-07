b7749dfa7151fd7c4f951d1fa6587fb4dbec7a57
Fernando Barros <fernandobarros@fernandos-mbp.lan>
2021-12-06 22:32:25 -0500

    Commiting after completing rock, paper, scissors game.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,44 +1,69 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = { r: 'rock',
+                  p: 'paper',
+                  sc: 'scissors',
+                  sp: 'spock',
+                  l: 'lizard' }
+
+WINNING_COMBOS = { rock: %w(scissors lizard),
+                   paper: %w(rock spock),
+                   scissors: %w(paper lizard),
+                   spock: %w(rock scissors),
+                   lizard: %w(spock paper) }
 
 def prompt(message)
   puts ">> #{message}"
 end
 
 def win?(player1, player2)
-  (player1 == 'rock' && player2 == 'scissors') ||
-    (player1 == 'paper' && player2 == 'rock') ||
-    (player1 == 'scissors' && player2 == 'paper')
+  WINNING_COMBOS[player1.to_sym].include?(player2)
 end
 
-def display_result(player, computer)
+def who_won?(player, computer)
   if win?(player, computer)
-    prompt('You won, computer lost.')
+    :player
   elsif win?(computer, player)
-    prompt('You lost, computer won.')
+    :computer
   else
-    prompt("It's a tie!")
+    :tie
   end
 end
 
-loop do
-  choice = ''
+def display_result(result)
+  round_messages = {
+    player: 'You won, computer lost.',
+    computer: 'You lost, computer won.',
+    tie: "It's a tie!"
+  }
+
+  prompt round_messages[result]
+end
+
+prompt("Welcome to #{VALID_CHOICES.values.join(', ')}!")
+prompt('The first player to reach three points wins')
+
+score = { player: 0,
+          computer: 0 }
+until score.max[1] == 3
+  choice = nil
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Choose one: #{VALID_CHOICES.values.join(', ')} \
+    (#{VALID_CHOICES.keys.join(', ')})")
     choice = gets.chomp.downcase
-    break if VALID_CHOICES.include?(choice)
+    choice =  VALID_CHOICES[choice.to_sym] if \
+              VALID_CHOICES.keys.include?(choice.to_sym)
+    break if VALID_CHOICES.values.include?(choice)
     prompt('Invalid choice.')
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.values.sample
 
   prompt("You chose #{choice}; Computer chose: #{computer_choice}")
 
-  display_result(choice, computer_choice)
-
-  prompt('Do you want to play again?')
-  answer = gets.chomp
-
-  break unless answer.downcase == 'y'
+  winner = who_won?(choice, computer_choice)
+  display_result(winner)
+  score[winner] += 1 unless winner == :tie
 end
+
+prompt("#{score.max[0].capitalize} won!")
 
 prompt('Thank you for playing, goodbye!')
