b465c14f78879945e9df634ab9ab8cabf8a4e23b
Fernando Barros <fernandobarros@fernandos-mbp.lan>
2021-12-09 19:57:48 -0500

    Commiting after completing Practice problems: easy 2

---
+++ lesson_3/e1_q6.rb
@@ -0,0 +1,5 @@
+famous_words = "seven years ago..."
+
+puts "Four scores and " + famous_words
+
+puts "Four scores and " << famous_words
---
+++ lesson_3/e2_q7.rb
@@ -0,0 +1,5 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.concat(%w(Done Hoppy))
+
+p flintstones
